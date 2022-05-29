6bcf2062858b5a7c4f821807e32edf95778a6669
Abolfazl Jelodar <jelodarabolfazl@gmail.com>
2022-05-30 01:27:12 +0430

    The part of add products by admin runs by rest now.

---
+++ admin/restful.py
@@ -0,0 +1,36 @@
+from bclib import edge
+
+from db import mongo_connention
+
+# This is options for connecting to edge
+options = {
+    "server": "localhost:5000",
+    "router": "restful"
+}
+
+app = edge.from_options(options)
+
+
+# Add products
+@app.restful_action()
+def add_product(context: edge.RESTfulContext):
+    print("Admin sent a data for adding products")
+    database = mongo_connention.get_db()
+    information = context.body
+    products = []
+    for item in information["products"]:
+        new_product = {
+            "name": item["name"],
+            "inventory": int(item["inventory"]),
+            "price": int(item["price"]),
+            "description": item["description"],
+            "deleted": 0,
+        }
+        products.append(new_product)
+    database.product.insert_many(products)
+    return {
+        "message": "products added successfully"
+    }
+
+
+app.listening()
